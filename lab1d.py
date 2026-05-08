d1a66fd7dd9bffa960681443c133acddd8348603
Aakash <ana56@myseneca.ca>
2026-05-08 10:16:37 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program will use integer objects and math operations.'''
+
+x = 10
+y = 2
+z = 5
+
+answer = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(answer))
